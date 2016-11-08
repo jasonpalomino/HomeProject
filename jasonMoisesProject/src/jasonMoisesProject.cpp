a9f0d20a1fcfa8c5a35fe73d8943dc20ed72c13e
jasonpalomino <jasonpalomino@192.168.1.54>
2016-11-07 19:01:16 -0800

    Initial checkin of JasonMoisesProject.

---
+++ jasonMoisesProject/src/jasonMoisesProject.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : jasonMoisesProject.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Sally.h"
+using namespace std;
+
+//Part of Tutorial 51
+
+int main(){
+    Sally a(34);
+    Sally b(21);
+    Sally c;
+
+    c=a+b;
+    cout << c.num << endl;
+}
